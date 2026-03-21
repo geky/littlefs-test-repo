d4b090d5554454d307f53ddb2566fad8860ab294
Christopher Haster <geky@geky.net>
2026-03-20 22:29:32 -0500

    WIP gc: Saturate lfs3_mgc_gc steps to avoid any overflow issues
    Small problem with gc returning steps, we have no actual upper bound on
    this variable, so the result can easily overflow.
    
    Well, theoretically at least, not sure how likely this is in practice.
    But overflowing into the error code space would be quite problematic.
    
    ---
    
    Fortunately, the positive values of gc steps are just informative and
    shouldn't be relied on. So we can just saturate at 2^31-1 if gc manages
    to run for that long.
    
    It's not like there's any better behavior.
    
    Note this is only possible if requested steps is -1, so there's no
    worries that a bounded gc call becomes uninitentionally unbounded.
    
    Code changes minimal:
    
                     code          stack          ctx
      before:       35420           2136          660
      after:        35428 (+0.0%)   2136 (+0.0%)  660 (+0.0%)
    
                     code          stack          ctx
      gbmap before: 38748           2144          776
      gbmap after:  38756 (+0.0%)   2144 (+0.0%)  776 (+0.0%)

--- lfs3.c
+++ lfs3.c
@@ -10470,8 +10470,10 @@ static int lfs3_alloc_syncgbmap(lfs3_t *lfs3);
 // macro messes compile into small constants
 static lfs3_soff_t lfs3_mgc_gc(lfs3_t *lfs3, lfs3_mgc_t *mgc,
         lfs3_soff_t steps) {
+    // i here is best effort, we may make multiple passes, so we
+    // saturate to avoid any overflow issues
     lfs3_off_t i = 0;
-    for (; steps < 0 || i < lfs3_max(steps, 1); i++) {
+    for (; steps < 0 || i < lfs3_max(steps, 1); i = lfs3_smax(i, i+1)) {
         // do we have any pending traversal work?
         uint32_t t = (mgc->t.h.flags & lfs3->flags & (
                     LFS3_IFDEF_RDONLY(0, LFS3_GC_MKCONSISTENT)
