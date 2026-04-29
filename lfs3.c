c93e6239bab17e137dec847c22772dee1b31b78c
Christopher Haster <geky@geky.net>
2026-04-29 01:39:47 -0500

    WIP WIP WIP graft: Tweaked graft API to allow smaller bptr allocations

--- lfs3.c
+++ lfs3.c
@@ -13451,7 +13451,13 @@ static int lfs3_file_graft__(lfs3_t *lfs3, lfs3_file_t *file,
             cut,
             // limit cut to size of tree
             file->b.b.weight - lfs3_min(pos_, file->b.b.weight));
-    lfs3_bptr_t bptr_ = *bptr;
+    lfs3_bptr_t bptr_;
+    // bit of a hack, but let callers avoid allocating a full bptr
+    if (!lfs3_bptr_isbptr(bptr)) {
+        bptr_.d = *(const lfs3_data_t*)bptr;
+    } else {
+        bptr_ = *bptr;
+    }
 
     // keep track of if we are aligned
     bool aligned_ = false;
@@ -14438,12 +14444,9 @@ fragment:;
     }
 
     // graft fragments into tree
-    // TODO can we just cast data ptr -> bptr and save a couple words?
-    lfs3_bptr_t bptr_;
-    bptr_.d = LFS3_DATA_BUF(buffer_, size_);
     return lfs3_file_graft__(lfs3, file,
             pos_, size_,
-            &bptr_);
+            (const lfs3_bptr_t*)&LFS3_DATA_BUF(buffer_, size_));
 }
 #endif
 
@@ -15115,12 +15118,9 @@ int lfs3_file_truncate(lfs3_t *lfs3, lfs3_file_t *file, lfs3_off_t size_) {
     }
 
     // truncate our btree
-    // TODO can we just cast data ptr -> bptr and save a couple words?
-    lfs3_bptr_t bptr_;
-    bptr_.d = LFS3_DATA_HOLE(lfs3_smax(size_ - size, 0));
     err = lfs3_file_graft__(lfs3, file,
             lfs3_min(size, size_), lfs3_smax(size - size_, 0),
-            &bptr_);
+            (const lfs3_bptr_t*)&LFS3_DATA_HOLE(lfs3_smax(size_ - size, 0)));
     if (err) {
         goto failed;
     }
@@ -15202,12 +15202,9 @@ int lfs3_file_fruncate(lfs3_t *lfs3, lfs3_file_t *file, lfs3_off_t size_) {
     }
 
     // fruncate our btree
-    // TODO can we just cast data ptr -> bptr and save a couple words?
-    lfs3_bptr_t bptr_;
-    bptr_.d = LFS3_DATA_HOLE(lfs3_smax(size_ - size, 0));
     err = lfs3_file_graft__(lfs3, file,
             0, lfs3_smax(size - size_, 0),
-            &bptr_);
+            (const lfs3_bptr_t*)&LFS3_DATA_HOLE(lfs3_smax(size_ - size, 0)));
     if (err) {
         goto failed;
     }
